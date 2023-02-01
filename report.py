bbdb1fabfbf132a45da60433e3d302814c0d086b
Zachary Evan Katz <zekatz@odin.ics.uci.edu>
2023-01-31 23:39:29 -0800

    added message

---
+++ report.py
@@ -0,0 +1,26 @@
+class Report:
+
+    def __init__(self):
+        self.word_frequencies = {}
+        self.longest_page = float('-inf')
+        self.unqiue_urls = set()
+    
+    def update_frequencies(frequencies: {str: int}):
+        
+        
+        for word, freq in frequencies.items():
+    
+    def _get_total_words (frequencies: {str: int}):
+
+        total = 0
+        for _, freq in frequencies.items():
+            total+=freq
+        return freq
+    
+    def print_report():
+
+
+        ##prints unique urls
+        ##
+
+
