40baca00e4cede7983a106c8c91c060af9857e15
Hamin Park <haminp@uci.edu>
2023-02-01 14:25:57 -0800

    add to skeleton

--- utils/simhash.py
+++ utils/simhash.py
@@ -1,10 +1,17 @@
 from utils.response import Response
+from urllib.parse import urlparse
 
 
 class Simhash():
-    def __init__(self) -> None:
+    _simhashes: dict[str, int]
+    _threshold: float
+
+    def __init__(self, threshold=0.9) -> None:
+        self._simhashes = dict()
+        self._threshold = threshold
+
+    def is_similar(self, resp: Response, freqs: dict[str, int]) -> bool:
         pass
 
-
-    def is_similar(self, resp: Response, threshold=0.9) -> bool:
+    def _compute_simhash(freqs: dict[str, int]) -> int:
         pass
