7958f639e63f2612e396f9b5ec4d36e70033e6f5
Hamin Park <haminp@uci.edu>
2023-02-01 14:00:18 -0800

    start of simhash

---
+++ utils/simhash.py
@@ -0,0 +1,10 @@
+from utils.response import Response
+
+
+class Simhash():
+    def __init__(self) -> None:
+        pass
+
+
+    def is_similar(self, resp: Response, threshold=0.9) -> bool:
+        pass
