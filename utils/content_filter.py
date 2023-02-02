f64e180444728a0d811f03a31087dc68c4bd22ae
Zachary Evan Katz <zekatz@circinus-95.ics.uci.edu>
2023-02-01 22:25:23 -0800

    added type annotations to content_filter.py

--- utils/content_filter.py
+++ utils/content_filter.py
@@ -1,20 +1,21 @@
 import utils.stopwords
 
+Token = str #type hints
 
-def _get_total_words(frequencies: {str: int}):
+def _get_total_words(frequencies: {Token: int}) -> int:
     total = 0
     for _, freq in frequencies.items():
         total+=freq
     return freq
 
-def _get_total_stopwords(frequencies):
+def _get_total_stopwords(frequencies: {Token:int}):
     total = 0
     for word, freq in frequencies.items():
         total += freq if word in stopwords.STOPWORDS else 0
     return freq
 
 
-def has_high_textual_information_content(frequencies, min_word_threshold = 50, stopword_ratio_threshold = .5) -> bool:
+def has_high_textual_information_content(frequencies: {Token: str}, min_word_threshold = 50, stopword_ratio_threshold = .5) -> bool:
     '''This function will evaluate whether or not a page has high textual information content
        based on a number of factors'''
 
