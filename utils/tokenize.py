7ec939ce802d2e38b7d1a3df8a04bd4b2c7e9146
Hamin Park <haminp@uci.edu>
2023-02-01 22:19:22 -0800

    rewrite of tokenizing functions

--- utils/tokenize.py
+++ utils/tokenize.py
@@ -1,45 +1,41 @@
-def _tokenize(page_content) -> [str]:
-    '''This function takes in a string representing the visible text content on a page,
-       and returns a list of tokens in that page
+from collections import defaultdict
+from utils.response import Response
+import re
+
+
+Token = str # for type hints
+
+
+def computeWordFrequencies(text_from_html: str) -> defaultdict[Token, int]:
     '''
-      
-    result = []
-
-    token = '' 
-    for char in page_content: #O(m) where m is the amount of characters in the line
-                
-        if char.isalnum() and char.isascii(): #since utf-8 is used, non-english characters 
-                                                          # need to be treated as delimeters, as the encoding
-                                                          #will not remove them
-            token+=char.lower() #makes every token lowercase so that later they are unified
-        else:
-            if token != '': #filters out empty strings
-                result.append(token) #adds the whole current token to the list
-                token = '' #starts over with an empty token
-        
-    return result
-
-def _computeWordFrequencies(tokens: [str]) -> {str:int}:
-    '''This function takes in a list of tokens and returns 
-       a dict that maps each token to its number of 
-       occurencies in the list, case-insensitive.
-        
-       The tokens are assumed to be all lower case as 
-       tokenize handles this
-
-       Overall, the complexity of this function is O(n) since
-       every token in a list of n tokens is iterated over,
-       and O(1) dict operations are completed for every
-       token
+    Takes in a string, splitting on any non-alphanumeric chars.
+    Returns a mapping of the tokens to their frequency.
     '''
+    frequencies = defaultdict(int)
+    tokens = _tokenize(text_from_html)
 
-    frequencies = {}
-    for token in tokens: #O(n) 
-        frequencies[token] = frequencies.get(token, 0) + 1 #O(1) + O(1) - dict operations are O(1)
-        #Could also use a default dict. get has a default return value of 0 if token is not in dict
+    for token in tokens:
+        frequencies[token] += 1
     return frequencies
 
-def compute_word_frequencies_for_page(page_content: str) -> {str:int}:
-    '''Returns a dictionary of tokens and their frequency for a string storing the contents of a page'''
 
-    return _computeWordFrequencies(_tokenize(page_content))
+def _tokenize(to_be_tokenized: str) -> list[Token]:
+    '''Tokenizes a string by splitting on any non-alphanumeric char.'''
+    tokens = list()
+    # while loop runs until EOF
+    for line in to_be_tokenized.split('\n'):
+        # tokenize file line by line
+        _tokenize_line(line, tokens)
+    return tokens
+
+def _tokenize_line(line: str, tokens: list[Token]) -> None:
+    '''Tokenizes a line. Helper for _tokenize.'''
+    pattern = r'[^a-zA-Z0-9]'   # matches any non-alphanumeric char
+    for token in re.split(pattern, line):   # split on non-alphanumeric chars
+        if _is_valid_token(token):
+            tokens.append(token.lower())
+
+def _is_valid_token(token: str) -> bool:
+    '''Checks if the token is valid.'''
+    # checks that string is alphanumeric
+    return token.isalnum()
