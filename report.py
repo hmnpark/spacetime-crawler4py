d26f4d0e94ab89f3e4a01b36da83175cadbd9712
Zachary Evan Katz <zekatz@circinus-34.ics.uci.edu>
2023-02-01 17:11:39 -0800

    completed print_report() in report.py

--- report.py
+++ report.py
@@ -2,17 +2,19 @@ from utils.stopwords import STOPWORDS
 class Report:
 
     COMMON_WORD_CUTOFF = 50
-
+    '''
+    This report will keep track of 50 most common words as well as the longest page in terms of words
+    but it will not handle the distinct urls found or the subdomain as the frontier and worker classes
+    both log that information
+    '''
     def __init__(self):
-        self._word_frequencies = {}
-        self._longest_page = 0
-        self._unique_urls = set()
+        self._word_frequencies = {} #this will keep track of total word frequencies 
+        self._longest_page = 0 
         self._longest_page_url = None
     
     def add_page(url, frequencies):
-
-        self._update_frequencies(frequencies)
-        self._update_longest_page(url, frequencies)
+        self._update_frequencies(frequencies) ##total frequencies will be updated
+        self._update_longest_page(url, frequencies) ##longest page will be updated
 
     def _update_frequencies(frequencies: {str: int}):
         
@@ -22,7 +24,7 @@ class Report:
     def _get_total_words (frequencies: {str: int}):
 
         total = 0
-        for _, freq in frequencies.items():
+        for _, freq in frequencies.items(): #word here doesnt matter
             total+=freq
         return freq
     
@@ -31,17 +33,24 @@ class Report:
 
         if page_length > self._longest_page:
             self._longest_page = page_length
-            self._longest_page_url = url
+            self._longest_page_url = url #url will be tracked
 
     def _get_most_common_words(n) -> list:
-        sorted_freqs = sorted(frequencies.items(), key = (lambda x: (-x[1], x[0]))): 
-        return [word for word in sorted_freqs[:n] if word not in stopwords.STOPWORDS]
+        sorted_freqs = sorted(frequencies.items(), key = (lambda x: (-x[1], x[0]))): #sorts by frequency and order ties by alphabteical order
+        return [(word,freq) for (word,freq) in sorted_freqs[:n] if word not in stopwords.STOPWORDS] #checks to see if there are common stopwords and excludes such words
 
     def print_report():
+
+        ##maybe get unqiue pages from the frontier - this would be a convenience thing
+        print("REPORT:")
+        print(f'The longest page in terms of words was {self._longest_page_url} with {self._longest_page} words.\n')
+        print("The 50 most common words (ingoring English stopwords were:")
+        for w,f in _get_most_common_words(COMMON_WORD_CUTOFF):
+            print(f'{w} --> {f}')
         
         
 
-        ##prints unique urls
-        ##
 
 
+        
+        
