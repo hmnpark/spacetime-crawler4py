d957582b41643fb32283421ab1efc9c44a728e3b
Hamin Park <haminp@uci.edu>
2023-02-02 16:43:13 -0800

    fix bugs

--- utils/robots.py
+++ utils/robots.py
@@ -1,13 +1,21 @@
 import time
 from logging import Logger
-from urllib import urlparse
+from urllib.parse import urlparse
 
 from utils.response import Response
 from utils.config import Config
 from utils.download import download
 
 
-def robots_check(url: str, rules: dict[str, tuple[list[str], list[str]]], config: Config, logger: Logger) -> list[str]:
+# rules example dict:
+#   dict{netloc: ([list of allowed paths], [list of disallowed paths])}
+
+
+def robots_check(
+    url: str,
+    rules: dict[str, tuple[list[str], list[str]]],
+    config: Config, logger: Logger=None
+    ) -> list[str]:
     '''
     If the urls authority is new, try and process the robots.txt file.
     Return the given url if it respects the rules and any new sitemap urls.
@@ -22,8 +30,8 @@ def robots_check(url: str, rules: dict[str, tuple[list[str], list[str]]], config
 
     resp = download(f'{parsed.scheme}://{parsed.netloc}/robots.txt', config, logger)
     time.sleep(config.time_delay)
-    if resp != 200:
-        rules[parsed.netloc] = ([], []) # no robots.txt rules to follow. allow all.
+    rules[parsed.netloc] = ([], []) # no robots.txt rules to follow. allow all.
+    if resp.status != 200:
         return [url]    # no robots.txt to process
     
     urls = _process_robots(resp, rules)
@@ -33,10 +41,15 @@ def robots_check(url: str, rules: dict[str, tuple[list[str], list[str]]], config
     return urls
 
 
-def respects_robots(url, rules):
+def respects_robots(
+    url: str,
+    rules: dict[str, (list[str], list[str])]
+    ) -> bool:
+    '''Checks if the url respects its authority's robots.txt.'''
     parsed = urlparse(url)
     netloc = parsed.netloc
     if netloc not in rules:
+        # the rules will be updated in Frontier.add_url
         return True
     
     allowed = rules[netloc][0]
@@ -50,11 +63,27 @@ def respects_robots(url, rules):
                 )
     return True # not disallowed anywhere, so it respects robots.txt
 
-def _process_robots(resp: Response, rules: dict[str, tuple[list[str], list[str]]]) -> list[str]:
+
+def _process_robots(
+    resp: Response,
+    rules: dict[str, tuple[list[str], list[str]]]
+    ) -> list[str]:
+    '''
+    Add an authority's robot.txt rules to the dict.
+    Return a list of sitemap links.
+    '''
     extracted_urls = list()
     netloc = urlparse(resp.url).netloc
-    for line in resp.raw_response.text.split('\n'):
-        match line.split():
+    lines = resp.raw_response.text.split('\n')
+
+    # find correct user-agent
+    start_from_idx = 0
+    for i in range(len(lines)):
+        if lines[i] == 'User-agent: *':
+            start_from_idx = i + 1 
+    # start processing from user-agent
+    for i in range(start_from_idx, len(lines)):
+        match lines[i].split():
             case ['Sitemap:', site]:
                 extracted_urls.append(site)
             case ['Allow:', path]:
